b9d396fcdb8430893c83a479f206278290f31e4d
pgorner <pgorner@student.42heilbronn.de>
2023-03-27 15:46:09 +0200

    fixes for stuff

--- src/game/game.c
+++ src/game/game.c
@@ -6,7 +6,7 @@
 /*   By: pgorner <pgorner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 19:37:45 by pgorner           #+#    #+#             */
-/*   Updated: 2023/03/27 12:44:48 by pgorner          ###   ########.fr       */
+/*   Updated: 2023/03/27 15:44:57 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 void	set_values(t_m *m)
 {
-	m->camera->pos.x = 2 * m->x / m->window_w - 1; //pos = camera_x
-	m->camera->ray_dir.x = m->camera->ray_dir.x + m->camera->plane.x * m->camera->pos.x;
-	m->camera->ray_dir.y = m->camera->ray_dir.y + m->camera->plane.y * m->camera->pos.x;
+	m->camera->pos.x = 2 * m->x / (double)m->window_w - 1; //pos = camera_x
+	m->camera->ray_dir.x = m->map->player->dir.x + m->camera->plane.x * m->camera->pos.x;
+	m->camera->ray_dir.y = m->map->player->dir.y + m->camera->plane.y * m->camera->pos.x;
 	m->camera->map_x = (int)m->camera->ray_pos.x;
 	m->camera->map_y = (int)m->camera->ray_pos.y;
 }
@@ -132,26 +132,26 @@ void	movspeed(t_m *m)
 	m->time = mlx_get_time();
 	m->frametime = (m->time - m->old_time) / 1000;
 	// printf("FPS:%f\n", (1.0/m->frametime));
-	m->camera->mspeed = m->frametime * 0.1;
-	m->camera->rspeed = m->frametime * 0.01;
+	m->camera->mspeed = m->frametime * 5;
+	m->camera->rspeed = m->frametime * 3;
 }
 
 void	movement(t_m *m)
 {
 	if (mlx_is_key_down(m->map->mlx, MLX_KEY_UP))
 	{
-		if (m->map->map[(int)(m->map->player->pos.x + m->camera->ray_dir.x * m->camera->mspeed)][(int)m->camera->pos.y] == FALSE)
-			m->map->player->pos.x += m->camera->ray_dir.x * m->camera->mspeed;
-		if (m->map->map[(int)m->camera->pos.x][(int)(m->camera->pos.y + m->camera->ray_dir.y * m->camera->mspeed)] == FALSE)
-			m->map->player->pos.y += m->map->player->pos.y - m->camera->ray_dir.y * m->camera->mspeed;
+		if (m->map->map[(int)(m->map->player->pos.x + m->map->player->dir.x * m->camera->mspeed)][(int)m->map->player->pos.y] == FALSE)
+			m->map->player->pos.x += m->map->player->dir.x * m->camera->mspeed;
+		if (m->map->map[(int)m->map->player->pos.x][(int)(m->map->player->pos.y + m->camera->ray_dir.y * m->camera->mspeed)] == FALSE)
+			m->map->player->pos.y += m->map->player->pos.y - m->map->player->dir.y * m->camera->mspeed;
 		m->x = 0;
 	}
 	if (mlx_is_key_down(m->map->mlx, MLX_KEY_DOWN))
 	{
-		if (m->map->map[(int)(m->map->player->pos.x - m->camera->ray_dir.x * m->camera->mspeed)][(int)m->camera->pos.y] == FALSE)
-			m->map->player->pos.x -= m->camera->ray_dir.x * m->camera->mspeed;
-		if (m->map->map[(int)m->camera->pos.x][(int)(m->camera->pos.y - m->camera->ray_dir.y * m->camera->mspeed)] == FALSE)
-			m->map->player->pos.y -= m->map->player->pos.y - m->camera->ray_dir.y * m->camera->mspeed;
+		if (m->map->map[(int)(m->map->player->pos.x - m->map->player->dir.x * m->camera->mspeed)][(int)m->map->player->pos.y] == FALSE)
+			m->map->player->pos.x -= m->map->player->dir.x * m->camera->mspeed;
+		if (m->map->map[(int)m->map->player->pos.x][(int)(m->map->player->pos.y - m->map->player->dir.y * m->camera->mspeed)] == FALSE)
+			m->map->player->pos.y -= m->map->player->pos.y - m->map->player->dir.y * m->camera->mspeed;
 		m->x = 0;
 	}
 	if (mlx_is_key_down(m->map->mlx, MLX_KEY_LEFT))
